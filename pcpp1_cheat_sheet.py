4c1959b97526311d02f55e35405fae41ed52381a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 13:45:10 +0100

    Modified  pcpp1_cheat_sheet.py

--- pcpp1_cheat_sheet.py
+++ pcpp1_cheat_sheet.py
@@ -1,9 +1,10 @@
 class Test:
     def __init__(self) -> None:
         # Define a protected instance attribute starts
-        # with single _ it can be called outside the class
+        # with single _ it can be called outside the object
         # and its subclasses
         self._protected_attribute
 
-        #
+        # Private attributes start with double __ and it can' be
+        # called directly from outside the object
         self.__private_attribute
