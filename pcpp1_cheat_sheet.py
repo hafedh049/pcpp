7c9498ea38666e7eea63d3875b4d56f4517e4c1f
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-04 13:43:54 +0100

    Modified  pcpp1_cheat_sheet.py

--- pcpp1_cheat_sheet.py
+++ pcpp1_cheat_sheet.py
@@ -1,4 +1,6 @@
 class Test:
     def __init__(self) -> None:
-        # Define a protected instance attribute
+        # Define a protected instance attribute starts
+        # with single _ it can be called outside the class
+        # and its subclasses
         self._protected_attribute
