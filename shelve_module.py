3cd073b23dc7c44dff2cc20f1c3ba54442197119
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-11 10:41:54 +0100

    Modified  shelve_module.py

--- shelve_module.py
+++ shelve_module.py
@@ -8,5 +8,6 @@ with shelve.open("test") as shelvi:
     print(shelvi["username"])
 
 """
-shelve is a module in python that acts like dictionnary but it is persistent it means that
+shelve is a module in python that acts like dictionnary but it is persistent it means that it stores data on disk you can call also sync() to synchronize the changes
+it has an open method using it with the "with" keyword and it will call .close() automaticaly after the program finishes
 """
