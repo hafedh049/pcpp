bfc93006a8fe957584bebafd7b8132ceb906f8a6
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-20 17:39:30 +0100

    ModifiedM python_core.py

---
+++ python_core.py
@@ -0,0 +1,3 @@
+"""
+The '+' operator is in fact converted to the __add__() method and the len() function is converted to the __len__() method. These methods must be delivered by a class (now it’s clear why we treat classes as blueprints) to perform the appropriate action.
+"""
