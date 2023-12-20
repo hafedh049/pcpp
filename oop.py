fab9c1714dd690ee5ce789f2e3514fdd2b5f0071
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-20 16:42:14 +0100

    Modified  oop.py

--- oop.py
+++ oop.py
@@ -1,3 +1,4 @@
+"""
 from typing import override
 
 
@@ -46,7 +47,7 @@ class Cat(Animal):
     @override
     def speak():
         print("Meow")
-
+"""
 
 # -------------------------------------------------------
 """
@@ -61,4 +62,26 @@ Object: more general that instance, object treferi l particular instance of a cl
 """
 Ay class yextendi ml 'type' hak aleh ki tji tchouf fl type ta3 class tilgeh 'type'
 """
-print(type(Animal))
+
+
+class Duck:
+    def __init__(self, height, weight, sex):
+        self.height = height
+        self.weight = weight
+        self.sex = sex
+
+    def walk(self):
+        pass
+
+    def quack(self):
+        return print("Quack")
+
+
+duckling = Duck(height=10, weight=3.4, sex="male")
+drake = Duck(height=25, weight=3.7, sex="male")
+hen = Duck(height=20, weight=3.4, sex="female")
+
+print(Duck.__class__)
+print(duckling.__class__)
+print(duckling.sex.__class__)
+print(duckling.quack.__class__)
