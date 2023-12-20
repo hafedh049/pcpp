85b9bbc90a2f374362a32e7244dd6e979c4ce294
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-20 17:01:33 +0100

    ModifiedM oop.py

--- oop.py
+++ oop.py
@@ -108,3 +108,26 @@ This kind of variable exists when and only when it is explicitly created and add
 
 Each object carries its own set of variables – they don't interfere with one another in any way. The word instance suggests that they are closely connected to the objects (which are class instances), not to the classes themselves. To get access to the instance variable, you should address the variable in the following way: objectdotvariable_name.
 """
+
+# ----------------------------------------------------------
+
+# Another snippet shows that instance variables can be created during any moment of an object's life. Moreover, it lists the contents of each object, using the built-in __dict__ property that is present for every Python object.
+
+
+class Demo:
+    def __init__(self, value):
+        self.instance_var = value
+
+
+d1 = Demo(100)
+d2 = Demo(200)
+
+d1.another_var = "another variable in the object"
+
+print("contents of d1:", d1.__dict__)
+print("contents of d2:", d2.__dict__)
+
+# contents of d1: {'instance_var': 100, 'another_var': 'another variable in the object'}
+# contents of d2: {'instance_var': 200}
+
+# ----------------------------------------------------------
