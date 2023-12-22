d3e640c4017a9a553980aee6a17b5e39faef8a87
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 09:34:37 +0100

    ModifiedM serialization.py  M shelve_module.py

--- serialization.py
+++ serialization.py
@@ -218,3 +218,12 @@ print(type(data))
 print(data)
 print(data.size)
 print(data.get_size())
+
+
+"""
+A few additional words about the pickle module:
+
+it’s a Python implementation of the serialization process, so the pickled data cannot be exchanged with programs written in other languages like Java or C++. In such cases, you should think about the JSON or XML formats, which could be less convenient than pickling, but when assimilated are more powerful than pickling;
+the pickle module is constantly evolving, so the binary format may differ between different versions of Python. Pay attention that both serializing and deserializing parties are making use of the same pickle versions;
+the pickle module is not secured against erroneous or maliciously constructed data. Never unpickle data received from an untrusted or unauthenticated source.
+"""
--- shelve_module.py
+++ shelve_module.py
@@ -13,3 +13,13 @@ it has an open method using it with the "with" keyword and it will call .close()
 it has all the methods of a dictionnary
 3 files are created .bak,.dat,.dir
 """
+
+
+"""
+Serialization of Python objects using the shelve module
+As you remember, the pickle module is used for serializing objects as a single byte stream. Both serializing and deserializing parties must abide by the order of all the elements placed into a file or database, or sent via a network.
+
+There is another handy module, called shelve, that is built on top of pickle, and implements a serialization dictionary where objects are pickled and associated with a key. The keys must be ordinary strings, because the underlying database (dbm) requires strings.
+
+Therefore, you can open your shelved data file and access your pickled objects via the keys the way you do when you access Python dictionaries. This could be more convenient for you when you’re serializing many objects.
+"""
