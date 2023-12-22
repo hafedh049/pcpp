48fce653314f025d73c21b5f87dbd7982775718e
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 08:49:11 +0100

    ModifiedM deep_shallow_copy.py

--- deep_shallow_copy.py
+++ deep_shallow_copy.py
@@ -5,3 +5,21 @@ At first, an object (a list in this example) is created in the computer's memory
 then the object is populated with other objects. Now our object has a value;
 finally a variable, which you should treat as a label or name binding, is created, and this label refers to a distinct place in the computer memory.
 """
+
+
+"""
+What is that object 'identity'? Why are the object value and label not enough?
+
+The built-in id() function returns the 'identity' of an object. This is an integer which is guaranteed to be unique and constant for this object during its lifetime. Two objects with non-overlapping lifetimes may have the same id() value.
+
+CPython implementation detail: This is the address of the object in the memory. Don’t treat it as an absolute memory address.
+"""
+
+
+"""
+This function is rarely used in applications. More often you’ll use it to debug the code or to experiment while copying objects. The side effect of this infrequent use is that some developers forget about its existence and create their own variables titled id to store some kind of identity or identifier.
+
+As a result, a variable called id shadows the genuine function and makes it unreachable in the scope in which the variable has been defined. You should remember to avoid such situations!
+"""
+
+# priority = variable -> function
