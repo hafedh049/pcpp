dff8358aff9a554ae6711d6544549a0ea17ba381
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 09:00:11 +0100

    Modified  deep_shallow_copy.py

--- deep_shallow_copy.py
+++ deep_shallow_copy.py
@@ -107,3 +107,9 @@ constructs a new compound object and then, recursively, inserts copies into it o
 takes more time to complete, as there are many more operations to be performed;
 is implemented by the deepcopy() function, delivered by the python 'copy' module
 """
+
+"""
+The 'copy' module contains a function for shallow copying: copy(). Of course, you could say that for copying lists there is already the [:] notation, or a_list=list(b_list), and for dictionaries you could use a_dict = dict(b_dict).
+
+But think about making use of polymorphism when you need a universal function to copy any type object, so that in that case using a copy() function is the smart way to accomplish the task.
+"""
