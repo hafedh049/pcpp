bfc4ca634f1a3bf7371d23649fb5523b96e8f767
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 11:12:38 +0100

    ModifiedM metacasses.py

--- metacasses.py
+++ metacasses.py
@@ -263,3 +263,14 @@ Congratulations! You have just examined you first metaclass!
 
 
 """
+
+
+"""
+The concept of metaclasses looks hard at first glance, but if you’re responsible for API design or development, metaclasses are the magic that could help you in your work.
+
+When you want to change your classes automatically, but decorators are not efficient, then metaclasses should help you.
+
+In all other cases, you should agree with Tim Peters that you shouldn’t worry about metaclasses, because many problems can be solved in a simpler way than by using metaclasses.
+
+But still, it’s beneficial to understand metaclasses in order to know when to employ them to solve a class type problem.
+"""
