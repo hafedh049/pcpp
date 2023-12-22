1616220994b5bb51c91bfdbfc8cb58bcced5c935
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 08:47:11 +0100

    Modified  deep_shallow_copy.py

---
+++ deep_shallow_copy.py
@@ -0,0 +1,7 @@
+"""
+(Note that an assignment statement is being used, so evaluation of the right side of the clause takes precedence over the left side.)
+
+At first, an object (a list in this example) is created in the computer's memory. Now the object has its identity;
+then the object is populated with other objects. Now our object has a value;
+finally a variable, which you should treat as a label or name binding, is created, and this label refers to a distinct place in the computer memory.
+"""
