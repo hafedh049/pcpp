6c37b8871eeee4c03fd0cd3e74c9a5ef5d85af66
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 09:06:37 +0100

    AddedM serialization.py

---
+++ serialization.py
@@ -0,0 +1,13 @@
+"""
+Serialization of Python objects using the pickle module
+In this section, you will learn how to persist Python objects for later use.
+
+Pickling is the process of preserving or extending the lifespan of food. The resulting food is called a pickle, and to prevent ambiguity, prefaced with the 'pickled' adjective.
+
+
+jar
+
+Have you ever considered saving the output of your data processing for later use?
+
+The simplest way to persist outcomes is to generate a flat text file and to write your outcomes. It’s a very simple thing to do way which is not suitable for persisting sets of different types of objects or nested structures.
+"""
