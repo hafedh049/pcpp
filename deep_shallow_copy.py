36d6b8c9adf8e37e20dc887395326f2a548e4432
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-12-22 08:51:12 +0100

    ModifiedM deep_shallow_copy.py

--- deep_shallow_copy.py
+++ deep_shallow_copy.py
@@ -23,3 +23,21 @@ As a result, a variable called id shadows the genuine function and makes it unre
 """
 
 # priority = variable -> function
+
+
+"""
+When you have two variables referring to the same object, the return values of the id() function must be the same.
+"""
+
+
+"""
+What is the difference between the '==' and 'is' operators?
+
+What should you do to compare two objects?
+
+In order to compare two objects, you should start with the '==' operator as usual. This operator compares the values of both operands and checks for value equality. So here we witness a values comparison.
+
+In fact, two distinct objects holding the same values could be compared, and the result would be 'True'. Moreover, when you compare two variables referencing the same object, the result would be also 'True'.
+
+To check whether both operands refer to the same object or not, you should use the 'is' operator. In other words, it responds to the question: “Are both variables referring to the same identity?”
+"""
